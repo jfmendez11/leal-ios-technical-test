a0ff58b441672783439eda8d68e8b672a5b4960f
jfmendez11 <jf.mendez11@uniandes.edu.co>
2020-07-07 18:13:10 -0500

    Initial Commit

---
+++ leal-ios-technical-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  leal-ios-technical-test
+//
+//  Created by Juan Felipe Méndez on 7/07/20.
+//  Copyright © 2020 Juan Felipe Méndez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
